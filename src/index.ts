b2276f8c0cbc44a6d7f9c52c2d5fb99dd61ac75a
a.mitu <a.mitu@sourcepulp.com>
2024-07-22 08:50:41 +0100

    initial project setup with ts and react templates

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+import readLineSync from "readline-sync";
+import path from "path";
+import fse from "fs-extra";
+
+const NO_CHOICE_MADE = -1;
+const CURR_DIR = process.cwd();
+const templatesDir = path.join(__dirname, "../templates");
+const templates = fse.readdirSync(templatesDir);
+
+if (!templates.length) {
+  console.log("no template to choose from , templates folder is empty");
+  process.exit(0);
+}
+
+const index = readLineSync.keyInSelect(templates);
+
+if (index === NO_CHOICE_MADE) {
+  process.exit(0);
+}
+
+const projectName = readLineSync.question(
+  "What is the name of your project? ",
+  {
+    limit: (input) => input.trim().length > 0,
+    limitMessage: "The project has to have a name, try again",
+  }
+);
+
+const confirmCreateDirectory = readLineSync.keyInYN(
+  `You entered '${projectName}', create directory with this name?`
+);
+
+if (confirmCreateDirectory) {
+  const template = templates[index];
+  const source = path.join(templatesDir, template);
+  const destination = path.join(CURR_DIR, projectName);
+  fse
+    .copy(source, destination)
+    .then(() => console.log(`Successfully created ${destination}`))
+    .catch((err) => console.error(err));
+} else {
+  console.log("Aborted creating a new template");
+}
---
+++ templates/vanilla-ts-extension/src/background.ts
@@ -0,0 +1,10 @@
+async () => {
+	const [tab] = await chrome.tabs.query({
+		active: true,
+		currentWindow: true,
+	  });
+	chrome.scripting.executeScript({
+	  target: { tabId: tab.id ? tab.id : -1 },
+	  func: () => console.log("Hello from background script"),
+	});
+}
